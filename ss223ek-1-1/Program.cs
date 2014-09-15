24d31afb4c64dcdb9d46075ef3653056f22ff10f
ss223ek <ss223ek@student.lnu.se>
2014-09-15 17:56:40 +0200

    test

---
+++ ss223ek-1-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ss223ek_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mata in totalsumma och betalbelopp, kontrollera så inget är fel i datatyp
+
+            // om totalsumma avrundas till noll bryts allt
+
+            // om betalbelopp är för litet bryta allt
+
+            // beräkna öresavrundning och hur mycket som ska ges tillbaka
+
+            // Skriv ut kvitto
+
+
+            //beräkna hur många av varje mynt och sedel som detta kan ges tillbaka i
+
+            //har kommer en sista rad v1
+
+
+
+        }
+    }
+}
