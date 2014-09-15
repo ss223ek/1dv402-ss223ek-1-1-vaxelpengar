f908bf93fd86d95d4bc51d165c655d809486be54
ss223ek <ss223ek@student.lnu.se>
2014-09-15 20:31:53 +0200

    Påbörjad kod, ej provkörd ännu
    Signed-off-by: ss223ek <ss223ek@student.lnu.se>

--- ss223ek-1-1/Program.cs
+++ ss223ek-1-1/Program.cs
@@ -10,13 +10,61 @@ namespace ss223ek_1_1
     {
         static void Main(string[] args)
         {
+            uint total;
+            double subtotal, roundingOffAmount, payedAmount;
+            
             //mata in totalsumma och betalbelopp, kontrollera så inget är fel i datatyp
+           while (true)
+	       {
+	         try
+            {
+                Console.Write("Ange totalsumman     :");
+                subtotal=double.Parse(Console.ReadLine());
+                break;
+            }
+            catch
+            {
+                Console.Write("Något blev fel, mata in ett tal");   
+            }   
+           }
 
-            // om totalsumma avrundas till noll bryts allt
+
+           // om totalsumma avrundas till noll bryts allt
+
+           total = (uint)Math.Round(subtotal);
+            if(total<1)
+            {
+                Console.Write("Totalsumman är för liten! Köpet kunde inte genomföras.");
+                break;
+            }
+            
+
+            while (true)
+	       {
+	         try
+            {
+                Console.Write("Ange erhållet belopp     :");
+                payedAmount=double.Parse(Console.ReadLine());
+                break;
+            }
+            catch
+            {
+                Console.Write("Något blev fel, mata in ett tal");   
+            }   
+           }
+
+
+            
+          
 
             // om betalbelopp är för litet bryta allt
+            if(payedAmount<total)
+            {
+                Console.Write("Erhållet belopp är för litet. Köpet kunde inte genomföras.");
+            }
 
             // beräkna öresavrundning och hur mycket som ska ges tillbaka
+                roundingOffAmount = total - subtotal;
 
             // Skriv ut kvitto
 
